dd92db4ebe94184e5a32f9ad46a78ea6c867564e
Kevin Blues <kbluescode@gmail.com>
2015-05-22 14:47:27 -0700

    Simplified db:populate and migrates

--- db/migrate/20150521182307_create_users_table.rb
+++ db/migrate/20150521182307_create_users_table.rb
@@ -2,8 +2,8 @@ class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :email
       t.string :password
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20150521182955_create_stories_table.rb
+++ db/migrate/20150521182955_create_stories_table.rb
@@ -1,12 +1,8 @@
 class CreateStoriesTable < ActiveRecord::Migration
   def change
     create_table :stories do |t|
-      t.string :description
       t.string :tags
-      t.integer :total_upvotes
-      t.references :scene
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps null: false
     end
   end
 end
